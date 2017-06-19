31a6868a92263f89224d165e979573c62434a9aa
gabrielecker <gabriel.ecker@gmail.com>
2017-06-19 10:28:29 -0300

    Enhancement in inline query responses

--- bot/handler.py
+++ bot/handler.py
@@ -30,14 +30,14 @@ def on_inline_query(msg):
             InlineQueryResultArticle(
                 id='hoje', title='Cardápio de hoje',
                 input_message_content=InputTextMessageContent(
-                    message_text='Cardápio: {}'.format(get_menu('/hoje')),
+                    message_text=get_menu('/hoje'),
                     parse_mode='Markdown'
                 )
             ),
             InlineQueryResultArticle(
                 id='semana', title='Cardápio da semana',
                 input_message_content=InputTextMessageContent(
-                    message_text='*Cardápio*: {}'.format(get_menu('/semana')),
+                    message_text=get_menu('/semana'),
                     parse_mode='Markdown'
                 )
             )
