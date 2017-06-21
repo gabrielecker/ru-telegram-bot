80c4e67bff60c0c8126d02472f639e9157b0fa36
gabrielecker <gabriel.ecker@gmail.com>
2017-06-21 18:53:39 -0300

    Implementing some basic management in case of crawler fail

--- crawler/ru.py
+++ crawler/ru.py
@@ -1,5 +1,6 @@
 import requests
 from apscheduler.schedulers.blocking import BlockingScheduler
+from bot.logger import logger
 from bot.settings import RU_URL
 from bs4 import BeautifulSoup
 from crawler.db import redis
@@ -10,8 +11,12 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=8)
 def get_menu():
-    soup = BeautifulSoup(requests.get(RU_URL).content, 'html.parser')
+    page = requests.get(RU_URL)
+    if page.status_code != 200:
+        logger.warning('RU page was unavailable for some unknown reason')
+        return None
 
+    soup = BeautifulSoup(page.content, 'html.parser')
     for index, item in enumerate(soup.find_all('tr')[1:6]):
         week_day, daily_menu = item.find('td'), item.find_all('td')[1:]
         daily_menu[2].string = '*{}*'.format(daily_menu[2].string.strip())
