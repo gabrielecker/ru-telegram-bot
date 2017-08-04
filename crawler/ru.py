e222f1e5ec00fadd0de726a1de425359f94700be
gabrielecker <gabriel.ecker@gmail.com>
2017-08-04 16:39:06 -0300

    Fixing crawler data coherence and small issues with the formatter

--- crawler/ru.py
+++ crawler/ru.py
@@ -9,6 +9,28 @@ from crawler.db import redis
 sched = BlockingScheduler()
 
 
+def highlight(text):
+    return '*{}*'.format(text)
+
+
+def beautify(menu):
+    beautified = []
+    for item in menu:
+        beautified.append(item.get_text().strip())
+
+    beautified[2] = highlight(beautified[2])
+    return beautified
+
+
+def format_menu(index, day, items):
+    menu = beautify(items[(index * 6):(index * 6 + 6)])
+    daily_menu = '{} - {}'.format(
+        highlight(day.get_text().capitalize()),
+        ', '.join([item for item in menu])
+    )
+    return daily_menu
+
+
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=10)
 def get_menu():
     page = requests.get(RU_URL)
@@ -17,11 +39,10 @@ def get_menu():
         return None
 
     soup = BeautifulSoup(page.content, 'html.parser')
-    for index, item in enumerate(soup.find_all('tr')[1:6]):
-        week_day, daily_menu = item.find('td'), item.find_all('td')[1:]
-        daily_menu[2].string = '*{}*'.format(daily_menu[2].string.strip())
-        daily_menu = '*{}* - {}'.format(
-            week_day.get_text().capitalize(),
-            ', '.join([item.get_text().strip() for item in daily_menu])
-        )
-        redis.set(index, daily_menu)
+    items = soup.find_all('td', {'align': 'center'})
+    redis.flushdb()
+
+    for index in range(5):
+        week_day = soup.find_all('tr')[index + 1].find('td')
+        print(index, format_menu(index, week_day, items))
+        # redis.set(index, format_menu(index, week_day, items))
